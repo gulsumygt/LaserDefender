59e8d2d28a4014da7a1e776407f55f1741aed058
glsmygt <26656755+glsmygt@users.noreply.github.com>
2018-12-28 09:43:06 +0300

    Remove ignored files

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("Player")]
+    [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float padding = 1f;
+    [SerializeField] int health = 100;
+
+    [Header("Projectile")]
+    [SerializeField] GameObject laserPrefab;
+    [SerializeField] float laserSpeed = 10f;
+    [SerializeField] float laserDelay = 0.1f;
+
+    Coroutine fireCoroutine;
+
+    float minX;
+    float maxX;
+    float minY;
+    float maxY;
+    // Use this for initialization
+    void Start()
+    {
+        PositionToWorldPoint();
+    }
+
+    private void PositionToWorldPoint()
+    {
+        var camera = Camera.main;
+
+        minX = camera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+        maxX = camera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+        minY = camera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        maxY = camera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Fire();
+    }
+
+    private void Fire()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            fireCoroutine = StartCoroutine(ContinouslyFire());
+        }
+
+        if (Input.GetButtonUp("Fire1"))
+        {
+            StopCoroutine(fireCoroutine);
+        }
+    }
+
+    IEnumerator ContinouslyFire()
+    {
+        while (true)
+        {
+            var laser = Instantiate(laserPrefab, transform.position, Quaternion.identity);
+
+            laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, laserSpeed);
+
+            yield return new WaitForSeconds(laserDelay);
+        }
+    }
+
+    private void Move()
+    {
+        var deltax = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+
+        var newPosx = Mathf.Clamp(transform.position.x + deltax, minX, maxX);
+        var newPosY = Mathf.Clamp(transform.position.y + deltaY, minY, maxY);
+
+        transform.position = new Vector2(newPosx, newPosY);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        var damager = collider.GetComponent<Damager>();
+
+        if (damager == null) { return; }
+
+        GetDamage(damager);
+    }
+
+    private void GetDamage(Damager damager)
+    {
+        health -= damager.GetDamage();
+
+        damager.Hit();
+
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
